a6bdf7a4e578bcab72c2d6f366182544d392eb2a
Andrew Halberstadt <halbersa@gmail.com>
2012-03-30 12:02:48 -0400

    Add subreddit filtering mechanism

--- reddit-history/reddit-history.user.js
+++ reddit-history/reddit-history.user.js
@@ -15,8 +15,10 @@ var header = document.getElementById('header');
 var content = document.querySelectorAll('div.content')[0];
 var user = header.querySelectorAll('span.user')[0].getElementsByTagName('a')[0].innerHTML;
 var history = new History(user);
+
 append_history_tab();
 add_click_listeners();
+
 
 
 /**
@@ -35,7 +37,7 @@ function append_history_tab() {
 
     var reload = function(){
       location.reload();
-    }
+    };
 
     for (var i = 0; i < items.length; ++i){
       if (items[i].className == "selected"){
@@ -49,7 +51,27 @@ function append_history_tab() {
     content.innerHTML = "";
     var h_controls = document.createElement('div');
     h_controls.className = 'spacer historyControls';
-    var s="<input id='rhRegex' value=''/>";
+
+    var subs = history.get_subs();
+    var keys = [];
+    for (var k in subs) {
+      if (subs.hasOwnProperty(k)) {
+        keys.push(k);
+      }
+    }
+    keys.sort(function(a,b) {
+      if (a.toLowerCase() > b.toLowerCase())
+        return 1;
+      if (b.toLowerCase() > a.toLowerCase())
+        return -1;
+      return 0;
+    });
+
+    var s="<select id='rhSubs'><option selected value=''>All Subreddits</option>";
+    for (i = 0; i < keys.length; ++i) {
+      s+="<option value='" + subs[keys[i]] + "'>" + keys[i] + "</option>";
+    }
+    s+="</select> <input id='rhRegex' value=''/>";
     s+="<button id='rhFilter'>Filter (regex)</button>";
     s+="<button id='rhClear'>Clear All History</button>";
     s+="<div style='text-align: right; width: 50%; float: right;'>";
@@ -74,6 +96,10 @@ function append_history_tab() {
     h_case.addEventListener('click', function() {
       GM_setValue('ignorecase', h_case.checked);
     });
+
+    // Add select listener
+    var h_subs = document.getElementById('rhSubs');
+    h_subs.addEventListener('change', filter);
 
     var h_limit = document.getElementById('rhLimit');
     h_limit.value = GM_getValue('limit', 10000);
@@ -131,7 +157,7 @@ function add_handlers(s) {
 /**
  * Displays the history in the history tab
  */
-function show_history(regex) {
+function show_history(regex, sub) {
   var h_controls = content.querySelectorAll('div.historyControls')[0];
   var h_items = content.querySelectorAll('div.historyItems')[0];
   var re = null;
@@ -144,18 +170,20 @@ function show_history(regex) {
     var filter = document.getElementById('rhRegex');
     filter.value = regex;
   }
-  var items = history.get_history(re);
+  var items = history.get_history(re, sub);
+  var subs = history.get_subs();
 
   for (var i = 0; i < items.length; ++i) {
     var mod = (i % 2 == 0 ? 'even' : 'odd');
     var div = document.createElement('div');
     div.className = "thing link " + mod;
+
     var s = "<span class='rank' style='width:3.30ex;'>" + (i+1) + "</span>";
     s += "<div class='midcol' style='width:5ex;'><div class='arrow up'></div><div class='score unvoted'>•</div><div class='arrow down'></div></div>";
     s += "<div class='entry lcTagged' keyindex='" + i + "' style='margin-left:5px;'>";
     s += "<p class='title'><a class='title' href='" + items[i].url + "'>" + items[i].name + "</a>";
     s += "<span class='domain'> (<a href='" + items[i].domain_url + "'>" + items[i].domain_name + "</a>)</span></p>";
-    s += "<p class='tagline'>last accessed on " + items[i].accessed + ", in <a href='" + items[i].sub_url + "'>" + items[i].sub_name + "</a></p>";
+    s += "<p class='tagline'>last accessed on " + items[i].accessed + ", in <a href='" + subs[items[i].sub_name] + "'>" + items[i].sub_name + "</a></p>";
     s += "<ul class='flat-list buttons'><li class='first'><a class='comments' href='" + items[i].comments + "'>view comments</a></li>";
     s += "<li><a class='rhDelete' href='#'>delete</a></li></ul></div>";
     div.innerHTML = s;
@@ -184,8 +212,9 @@ function add_delete_listener(s, div) {
 function filter() {
   var h_items = content.querySelectorAll('div.historyItems')[0];
   var h_regex = document.getElementById('rhRegex');
+  var h_subs = document.getElementById('rhSubs');
   h_items.innerHTML = "";
-  show_history(h_regex.value);
+  show_history(h_regex.value, h_subs.options[h_subs.selectedIndex].innerHTML);
 }
 
 /**
@@ -218,19 +247,37 @@ function save_limit() {
  */
 function History(user) {
   this.key = "reddit_history_" + user;
-  if (localStorage.getItem(this.key) == null) {
-    localStorage.setItem(this.key, JSON.stringify([]));
+  this.subs = this.key + "_subs";
+
+  var items = localStorage.getItem(this.key);
+  if (items == null) {
+    items = [];
+    localStorage.setItem(this.key, JSON.stringify(items));
+  } else {
+    items = JSON.parse(items);
+  }
+
+  // Maintain backwards compatibility with older versions of the script
+  var subs = localStorage.getItem(this.subs);
+  if (subs == null) {
+    subs = {};
+    for (var i = 0; i < items.length; ++i) {
+      if ("sub_name" in items[i] && "sub_url" in items[i]) {
+        subs[items[i].sub_name] = items[i].sub_url;
+      }
+    }
+    localStorage.setItem(this.subs, JSON.stringify(subs));
   }
 }
 
-History.prototype.get_history = function(regex) {
+History.prototype.get_history = function(regex, sub) {
   limit = GM_getValue('limit', 10000);
   var submissions = JSON.parse(localStorage[this.key]);
   var count = 0;
   var ret = [];
   while (submissions.length > 0 && count <= limit) {
     var s = submissions.pop();
-    if (regex == null || regex.test(s.name)) {
+    if ((regex == null || regex.test(s.name)) && (sub == null || sub == "All Subreddits" || s.sub_name == sub)) {
       ret.push(s);
       count++;
     }
@@ -255,12 +302,15 @@ History.prototype.add_submission = function(name, url, comments, sub_name, sub_u
            'url': url,
            'comments': comments,
            'sub_name': sub_name,
-           'sub_url': sub_url,
            'domain_name': domain_name,
            'domain_url': domain_url,
            'accessed': new Date().toDateString()};
-  var submissions = JSON.parse(localStorage.getItem(this.key));
 
+  var subs = JSON.parse(localStorage.getItem(this.subs));
+  subs[sub_name] = sub_url;
+  localStorage.setItem(this.subs, JSON.stringify(subs));
+
+  var submissions = JSON.parse(localStorage.getItem(this.key));
   var index = this.indexOf(s);
   if (index != -1) {
     submissions.splice(index, 1);
@@ -273,17 +323,35 @@ History.prototype.add_submission = function(name, url, comments, sub_name, sub_u
 };
 
 History.prototype.remove_submission = function(comments) {
+  // The comment url uniquely identifies the submission
   var s = {'comments': comments};
-  var submissions = JSON.parse(localStorage.getItem(this.key));
-  
   var index = this.indexOf(s);
   if (index != -1) {
+    // Remove submission
+    var submissions = JSON.parse(localStorage.getItem(this.key));
+    var sub_name = submissions[index].sub_name;
     submissions.splice(index, 1);
-    localStorage[this.key] = JSON.stringify(submissions);
+    localStorage.setItem(this.key, JSON.stringify(submissions));
+   
+    // Remove subreddit entry 
+    for (var i = 0; i < submissions.length; ++i) {
+      if (submissions[i].sub_name == sub_name) {
+          return;
+      }
+    }
+    var subs = JSON.parse(localStorage.getItem(this.subs));
+    delete subs[sub_name];
+    localStorage.setItem(this.subs, JSON.stringify(subs));
   }
+};
+
+History.prototype.get_subs = function() {
+    return JSON.parse(localStorage.getItem(this.subs));
 };
 
 History.prototype.clear = function() {
   localStorage.setItem(this.key, JSON.stringify([]));
+  localStorage.setItem(this.subs, JSON.stringify({}));
 };
+
 
