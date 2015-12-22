1e5d6520499b3e2284bbcdb72bc0402daf31d4c6
Andrew Halberstadt <halbersa@gmail.com>
2015-12-22 17:55:12 -0500

    Initial fantrax player research script

---
+++ fantrax-player-research.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name           Fantrax Player Research
+// @namespace      tag:halbersa@gmail.com,2015-12-22:ahal
+// @description    Add more information to player pages in Fantrax
+// @include        *fantrax.com/playerProfile.go*
+// ==/UserScript==
+
+alert('hello world');
